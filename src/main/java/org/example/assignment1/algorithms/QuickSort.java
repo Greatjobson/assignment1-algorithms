87dc08bddc7f0379c96308527540a1227bc0a761
Greatjobson <zumabejba@gmail.com>
2025-09-28 17:10:13 +0500

    refactor(util): partition, swap, shuffle, guards

--- src/main/java/org/example/assignment1/algorithms/QuickSort.java
+++ src/main/java/org/example/assignment1/algorithms/QuickSort.java
@@ -1,18 +1,14 @@
 package org.example.assignment1.algorithms;
 
-import org.example.assignment1.metrics.MetricCollector;
-import java.util.Random;
+import org.example.assignment1.util.Util;
 
 public class QuickSort {
-    private MetricCollector mc = new MetricCollector();
-    private Random rand = new Random();
     private static final int INSERTION_SORT_THRESHOLD = 16;
 
     public void sort(int[] arr) {
         if (arr == null || arr.length == 0) return;
-        mc.startTimer();
+        Util.checkArray(arr);
         quickSort(arr, 0, arr.length - 1);
-
     }
 
     private void quickSort(int[] arr, int low, int high) {
@@ -21,8 +17,7 @@ public class QuickSort {
                 insertionSort(arr, low, high);
                 break;
             }
-            mc.enterRecursion();
-            int pivotIdx = partition(arr, low, high);
+            int pivotIdx = Util.partition(arr, low, high);
             if (pivotIdx - low < high - pivotIdx) {
                 quickSort(arr, low, pivotIdx - 1);
                 low = pivotIdx + 1;
@@ -30,44 +25,16 @@ public class QuickSort {
                 quickSort(arr, pivotIdx + 1, high);
                 high = pivotIdx - 1;
             }
-            mc.exitRecursion();
         }
-    }
-
-    private int partition(int[] arr, int low, int high) {
-        int pivotIdx = low + rand.nextInt(high - low + 1);
-        swap(arr, pivotIdx, high);
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            mc.incrementComparisons();
-            if (arr[j] <= pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 
     private void insertionSort(int[] arr, int low, int high) {
         for (int i = low + 1; i <= high; i++) {
             int key = arr[i];
             int j = i - 1;
-            while (j >= low) {
-                mc.incrementComparisons();
-                if (arr[j] > key) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                } else {
-                    break;
-                }
+            while (j >= low && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
             arr[j + 1] = key;
         }
--- src/main/java/org/example/assignment1/util/Util.java
+++ src/main/java/org/example/assignment1/util/Util.java
@@ -1,4 +1,55 @@
 package org.example.assignment1.util;
 
+import java.util.Random;
+
 public class Util {
+    private static final Random rand = new Random();
+
+    // Guards:
+    public static void checkArray(int[] arr) {
+        if (arr == null) throw new IllegalArgumentException("Array cannot be null");
+        if (arr.length == 0) throw new IllegalArgumentException("Array cannot be empty");
+    }
+
+    public static void checkArray(Point[] arr) {
+        if (arr == null) throw new IllegalArgumentException("Array cannot be null");
+        if (arr.length < 2) throw new IllegalArgumentException("Array must have at least 2 points");
+    }
+
+    // Swap:
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Shuffle:
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+    // Partition:
+    public static int partition(int[] arr, int low, int high) {
+        int pivotIdx = low + rand.nextInt(high - low + 1);
+        swap(arr, pivotIdx, high);
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // Closest Pair
+    public static class Point {
+        int x, y;
+        Point(int x, int y) { this.x = x; this.y = y; }
+    }
 }
